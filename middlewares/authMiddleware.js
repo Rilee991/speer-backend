b600dec979c11e8d3a13a8fafd51ecd25432d8c9
cypher33 <rohitkumar25.rkl@gmail.com>
2024-01-04 18:05:28 +0530

    completed middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const User = require("../src/models/User");
+
+const authenticateToken = (req, res, next) => {
+    const token = req?.cookies?.jwt;
+
+    if(!token) {
+        return res.status(401).json({ error: 'Unauthorized access' });
+    }
+
+    jwt.verify(token, process.env.SECRET_KEY, async (err, data) => {
+        if(err) {
+            return res.status(401).json({ error: 'Invalid user' });
+        }
+
+        const user = await User.findById(data.id, { password: 0 });
+
+        if(!user) return res.status(400).json({ error: true, message: "User doesnt exists." });
+
+        req.user = user;
+        console.log(user);
+        next();
+    });
+}
+
+module.exports = {
+    authenticateToken
+};
